38ec7e03ec1e81782edbd593f7107d85e295ce88
JeongHaeHyeon <wjdgo813@afreecatv.com>
2018-02-01 22:21:14 +0900

    정해현 [MooDumdum] - 첫 커밋 .. !

---
+++ MooDumDum/MooDumDum/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MooDumDum
+//
+//  Created by JHH on 2018. 2. 1..
+//  Copyright © 2018년 MooDumdum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func pressedMyInfoButton(_ sender: Any) {
+        
+    }
+}
+
